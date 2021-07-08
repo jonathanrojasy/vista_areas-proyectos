f1f775a40819c42308e92f40e94f42aab81389c7
jonathanrojasy <jonathanry98@gmail.com>
2021-07-08 16:17:33 -0500

    NextEvents: vista temporal

--- src/components/NextEvents.vue
+++ src/components/NextEvents.vue
@@ -9,6 +9,21 @@
           >
             <span class="text-h2 font-weight-regular mt-2">Próximos eventos</span>
           </v-row>
+          <v-row
+              align="center"
+              justify="center"
+          >
+            <v-sheet
+                height="500"
+                width="500"
+                outlined
+                rounded
+            >
+              <v-container fill-height fluid>
+                <span class="text-h4 font-weight-light text-center">Aquí van los próximos eventos.</span>
+              </v-container>
+            </v-sheet>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
