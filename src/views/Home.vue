c999592f032eebd7229fc6b0d38f13c1dcf5ee8d
jonathanrojasy <jonathanry98@gmail.com>
2021-07-08 22:05:42 -0500

    Implementación de rutas

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container fluid class="pa-0">
+    <Inicio/>
+    <AboutCCAT/>
+    <OurValues/>
+    <Areas/>
+    <Projects/>
+    <Products/>
+    <NextEvents/>
+  </v-container>
+</template>
+
+<script>
+import Inicio from "../components/Inicio";
+import AboutCCAT from "../components/AboutCCAT";
+import OurValues from "../components/About/OurValues";
+import Areas from "../components/Areas";
+import Projects from "../components/Projects";
+import Products from "../components/Products";
+import NextEvents from "../components/NextEvents";
+export default {
+  name: "Home",
+  components: {
+    Inicio,
+    AboutCCAT,
+    OurValues,
+    Areas,
+    Projects,
+    Products,
+    NextEvents,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
